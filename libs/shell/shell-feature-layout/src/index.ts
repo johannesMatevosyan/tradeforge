a3f1167bd58ace66d1864e17d1ba4391fa488b1b
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-04-07 19:36:29 +0400

    feat(shell): add main-shell component to shell layout library

--- libs/shell/shell-feature-layout/src/index.ts
+++ libs/shell/shell-feature-layout/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/lib.routes';
 
 export * from './lib/shell-feature-layout/shell-feature-layout';
+
+export * from './lib/main-shell/main-shell';
---
+++ libs/shell/shell-feature-layout/src/lib/main-shell/main-shell.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-main-shell',
+  imports: [],
+  templateUrl: './main-shell.html',
+  styleUrl: './main-shell.scss',
+})
+export class MainShell {}
