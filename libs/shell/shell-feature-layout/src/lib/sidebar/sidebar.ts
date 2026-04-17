c3d6c1c4b9a95914951b80d0557a4f7a196fa922
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-04-17 12:57:19 +0400

    feat(sidebar): add RouterLink import to sidebar component

--- libs/shell/shell-feature-layout/src/lib/sidebar/sidebar.ts
+++ libs/shell/shell-feature-layout/src/lib/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { AppIconComponent } from '../../../../../shared-ui-icons/src/lib/app-icon/app-icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [AppIconComponent],
+  imports: [AppIconComponent, RouterLink],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.scss'],
   standalone: true,
