dcc4596fe45ce309cdfa8c75d65e776dc64c3a59
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-04-21 19:27:08 +0400

    feat(forbidden): implement 403 Forbidden page with styling and navigation

--- apps/frontend/src/app/auth/pages/forbidden/forbidden.component.ts
+++ apps/frontend/src/app/auth/pages/forbidden/forbidden.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-forbidden',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './forbidden.component.html',
   styleUrl: './forbidden.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
