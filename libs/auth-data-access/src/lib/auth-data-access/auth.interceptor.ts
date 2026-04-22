04204d99175ab9b40f3476fae77b63fd044683e6
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-04-22 16:40:57 +0400

    feat(auth): enhance auth interceptor to handle 401 errors and redirect to login

--- libs/auth-data-access/src/lib/auth-data-access/auth.interceptor.ts
+++ libs/auth-data-access/src/lib/auth-data-access/auth.interceptor.ts
@@ -1,21 +1,35 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 import { AuthService } from './auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
     const authService = inject(AuthService);
+    const router = inject(Router);
     const token = authService.getToken();
 
     if (!token) {
         return next(req);
     }
 
-    const authReq = req.clone({
-        setHeaders: {
-        Authorization: `Bearer ${token}`,
-        },
-    });
+    const authReq = token
+        ? req.clone({
+            setHeaders: {
+            Authorization: `Bearer ${token}`,
+            },
+        })
+        : req;
 
-    return next(authReq);
+    return next(authReq).pipe(
+        catchError((error: HttpErrorResponse) => {
+            if (error.status === 401 && authService.isAuthenticated()) {
+                authService.logout();
+                router.navigateByUrl('/auth/login');
+            }
+
+            return throwError(() => error);
+        }),
+    );
 };
