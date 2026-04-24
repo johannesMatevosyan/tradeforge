bb8f954c54a3430b3c743025ede8dee7eb553137
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-04-24 20:51:18 +0400

    test(auth): update tests to use getCurrentUser method instead of currentUser

--- apps/frontend/src/app/auth/pages/forbidden/forbidden.component.spec.ts
+++ apps/frontend/src/app/auth/pages/forbidden/forbidden.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { ForbiddenComponent } from './forbidden.component';
 
 describe('ForbiddenComponent', () => {
@@ -8,6 +9,7 @@ describe('ForbiddenComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ForbiddenComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ForbiddenComponent);
