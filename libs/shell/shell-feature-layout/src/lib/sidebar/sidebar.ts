781b9da7593171a36625c62248a673667bebcc89
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-04-30 15:05:47 +0400

    feat(sidebar): enhance navigation item styles and import RouterLinkActive for active link highlighting

--- libs/shell/shell-feature-layout/src/lib/sidebar/sidebar.ts
+++ libs/shell/shell-feature-layout/src/lib/sidebar/sidebar.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AppIconComponent } from '../../../../../shared-ui-icons/src/lib/app-icon/app-icon';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [AppIconComponent, RouterLink],
+  imports: [AppIconComponent, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.scss'],
   standalone: true,
