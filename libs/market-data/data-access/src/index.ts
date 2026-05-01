6de8ae4eab60291a922451568c76066851823508
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-05-01 09:29:51 +0400

    fix: correct import path for MarketDataWsService in index.ts

--- libs/market-data/data-access/src/index.ts
+++ libs/market-data/data-access/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/market-price-with-direction';
 export * from './lib/services/market-data.api';
-export * from './lib/services/market-data.ws';
+export * from './lib/services/market-data.ws.service';
 
