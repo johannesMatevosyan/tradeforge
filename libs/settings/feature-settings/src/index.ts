241b7078af3b3fe36e60e3e3a7a4fcdd45088ee9
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-05-13 13:25:36 +0400

    feat(settings): initialize feature-settings library with routing, components, and testing setup

---
+++ libs/settings/feature-settings/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/settings-page/settings-page.component';
+export * from './lib/settings.routes';
+
---
+++ libs/settings/feature-settings/src/lib/settings-page/settings-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-settings-page',
+  imports: [],
+  standalone: true,
+  templateUrl: './settings-page.component.html',
+  styleUrl: './settings-page.component.scss',
+})
+export class SettingsPageComponent {}
