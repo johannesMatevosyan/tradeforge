138980b2e06bedc1edcacee1234b890ae28af65f
johannesMatevosyan <h.matevosyan@yahoo.com>
2026-05-14 23:18:06 +0400

    feat(shared-utils): add formatSymbolValue function to handle symbol formatting

--- libs/shared-utils/src/lib/symbol.utils.ts
+++ libs/shared-utils/src/lib/symbol.utils.ts
@@ -1,3 +1,13 @@
 export function normalizeSymbol(symbol: string): string {
   return symbol.replace('/', '').toUpperCase();
 }
+
+export function formatSymbolValue(symbol: string): string {
+  if (symbol.includes('/')) return symbol;
+
+  if (symbol.endsWith('USD')) {
+    return `${symbol.replace('USD', '')}/USD`;
+  }
+
+  return symbol;
+}
